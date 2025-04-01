f30c2f93029401969fe25c2a0f8f7876440fa45c
Ankit Ghimire <144568713+AnkitGhi@users.noreply.github.com>
2025-04-01 01:12:23 -0500

    Added analytics views

--- task/views.py
+++ task/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import Data_provide, Submission_Provide
-from .models import Task_register, Task_complete, TaskSummary
+from .models import Task_register, Task_complete, TaskSummary, Analytics
 from django.contrib.auth .decorators import login_required
 
 @login_required
@@ -49,3 +49,36 @@ def view_tasks(request):
     tasks = Task_register.objects.filter(user=request.user)
     
     return render(request, 'task/view_tasks.html', {'tasks': tasks})
+
+def analytics(request):
+    task_stored = TaskSummary.objects.filter(user=request.user)
+    task_incomplete = Task_register.objects.filter(user=request.user)
+
+    no_task_complete = task_stored.count()
+    no_task_incomplete = task_incomplete.count()
+    if no_task_complete > 0:
+        average_actual_time = sum(tasks.actual_time for tasks in task_stored)/no_task_complete
+        average_motivation = sum(tasks.moti for tasks in task_stored)/no_task_complete
+        average_seatings = sum(taks.num_of_seatings for taks in task_stored)/no_task_complete
+        average_esti_time = sum(tasks.est_time for tasks in task_stored)/no_task_complete
+    else:
+        average_esti_time = 0
+        average_actual_time = 0
+        average_motivation = 0
+        average_seatings = 0
+   
+    if request.method=='POST':
+        Analytics.objects.update_or_create(
+            user=request.user,
+            defaults={
+                'average_time_completion':average_actual_time,
+                'average_time_estimated': average_esti_time,
+                'no_task_completed': no_task_complete,
+                'no_task_remaining': no_task_incomplete,
+                'average_motivation': average_motivation, 
+                'average_seatings': average_seatings,
+            }
+        )
+
+
+
